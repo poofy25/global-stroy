c57b9f7eaf38f3702c4035b6113c022e8cf867d7
poofy25 <117314241+poofy25@users.noreply.github.com>
2023-12-23 21:37:56 +0200

    bug

--- src/pages/Work/workContent.jsx
+++ src/pages/Work/workContent.jsx
@@ -7,7 +7,6 @@ import albisoara804_06 from '/src/assets/img/work/albisoara80-4_06.png'
 import albisoara804_07 from '/src/assets/img/work/albisoara80-4_07.png'
 import albisoara804_08 from '/src/assets/img/work/albisoara80-4_08.png'
 
-
 import liviudeleanu_01 from '/src/assets/img/work/liviudeleanu_01.png'
 import liviudeleanu_02 from '/src/assets/img/work/liviudeleanu_02.png'
 import liviudeleanu_03 from '/src/assets/img/work/liviudeleanu_03.png'
@@ -20,13 +19,12 @@ import socoleni15_03 from '/src/assets/img/work/socoleni15_03.png'
 import mircea_cel_batran_41_img from '/src/assets/img/work/mircea-cel-batran-41.png'
 import mircea_cel_batran_41_vid from '/src/assets/img/work/mircea-cel-batran-41.mp4'
 
-
 const negruzii_5 = import.meta.glob('/src/assets/img/work/Negruzii_5/*.png');
 const grenoblea_120 = import.meta.glob('/src/assets/img/work/Grenoblea_120/*.png');
 const grenoblea_130 = import.meta.glob('/src/assets/img/work/Grenoblea_130/*.png');
 
 console.log (negruzii_5 , grenoblea_120 ,grenoblea_130)
-
+console.log (getImgs(negruzii_5) , getImgs(grenoblea_120) , getImgs(grenoblea_130))
 export const getImgs = (folder) => {
     const imagePaths = Object.keys(folder);
     return imagePaths;
