0183e60eaeac63201e943c0e0531f1b6c446c523
poofy25 <117314241+poofy25@users.noreply.github.com>
2023-12-23 21:58:57 +0200

    bug

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+module.exports = {
+  root: './',
+  build: {
+      outDir: 'dist',
+  },
+  publicDir: 'public'
+} 
